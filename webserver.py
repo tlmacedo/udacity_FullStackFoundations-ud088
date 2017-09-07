611dc1e9061d12a1e9d07431faba8d7a73ff0765
thiagomacedo <tl.macedo@hotmail.com>
2017-09-07 16:36:54 -0400

    auto commit

--- webserver.py
+++ webserver.py
@@ -1,3 +1,4 @@
+import cgi
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
@@ -9,9 +10,12 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 self.send_header('content-type', 'text/html')
                 self.end_headers()
 
-                output = b"<html><title>Hello meu amor</title><body>Hello!!!!</body></html>"
-                output = b"<html><title>Hello meu amor</title><body>Hello!!!!<a href='/hola'> vai para &#161&#161&#161Hola</a></body></html>"
-                self.wfile.write(output)  # .wfile(output) .wfile.write(output)
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='post' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text' required><input type='submit' value='Submeter'></form>'''
+                output += "</body></html>"
+                self.wfile.write(output.encode())  # .wfile(output) .wfile.write(output)
                 print(output)
                 return
 
@@ -20,28 +24,54 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 self.send_header('content-type', 'text/html')
                 self.end_headers()
 
-                output = b"<html><title>Hola mi amor</title><body>&#161&#161&#161Hola  <a href='/hello'> volta para Hello!!!!</a></body></html>"
-                self.wfile.write(output)  # .wfile(output) .wfile.write(output)
+                output = ""
+                output += "<html><body>"
+                output += "<h1>&#161 Hola !</h1>"
+                output += '''<form method='post' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text' required><input type='submit' value='Submeter'></form>'''
+                output += "</body></html>"
+                self.wfile.write(output.encode())  # .wfile(output) .wfile.write(output)
                 print(output)
                 return
 
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
+    def do_POST(self):
+        try:
+            ctype, pdict = cgi.parse_header(self.headers['content-type'])
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            output = ""
+            output += "<!DOCTYPE html><html lang='pt-br'><head><meta charset='UTF-8'>"
+            output += "<title>Hello meu amor</title></head><body>"
+            output += "<h2>Okay, how about this:</h2>"
+            output += "<h1> %s </h1>" % messagecontent[0].decode('utf-8')  # % str(messagecontent[0])
+            output += '''<form method='post' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submeter'></form>'''
+            output += "</body></html>"
+            self.send_response(301)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(output.encode('utf-8'))
+        except:
+            self.send_error("%d" % self.error_content_type)
+
 
 def main():
     try:
-        port = 8080
-        server = HTTPServer(('127.0.0.1', port), WebServerHandler)
-        # httpd = make_server('', 8080, WebServerHandler)
-        print("Executando web server HTTP na porta 8080")
-        server.serve_forever()
-        # httpd.serve_forever()
+        HOST, PORT = "127.0.0.1", 8080
+        with HTTPServer((HOST, PORT), WebServerHandler) as server:
+            # port = 8080
+            # server = HTTPServer(('127.0.0.1', port), WebServerHandler)
+            # httpd = make_server('', 8080, WebServerHandler)
+            print("Executando web server HTTP na porta 8080")
+            server.serve_forever()
+            # httpd.serve_forever()
 
     except KeyboardInterrupt:
         print("^C presionado, servidor interrompido...")
         server.socket.close()
-        # httpd.socket.close()
 
 
 if __name__ == '__main__':
